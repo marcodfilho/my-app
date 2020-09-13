aa0f55663eae5096125179ef8cb86b4c60b26a2e
Marco Filho <mdias29@gmail.com>
2020-09-13 12:22:22 -0300

    segundo commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Meu primeiro frontend - Marco
         </a>
       </header>
     </div>
